a6c01219684e6e820e25442f91ae6edcc4f65b3f
AliMurad-codes <162787735+AliMurad-codes@users.noreply.github.com>
2024-03-09 03:03:04 -0800

    Add files via upload

---
+++ Python Operators.py
@@ -0,0 +1,78 @@
+# Print in Python
+print("hello, python!")
+
+# Addition Operator
+
+print("2+2 =", (2+2))
+
+#subtraction operator
+print("3-2=", (3-2))
+
+# multiplication
+print("3*3=",(3*3))
+
+# division 
+print("4/2=",(4/2))
+
+#modulus
+print("9/2=",(9%2))
+
+#increment
+x=1
+x+=1
+print(x)
+
+#decrement 
+x=4
+x-=1
+print(x)
+
+#decrement  by divide
+x=8
+x/=4
+print(x)
+
+#decrement by multiply
+x=3
+x*=3
+print(x)
+
+# camparison(==)
+x=2
+print(x,x==2)
+
+# comparison less then equal 
+
+x=3
+x = x<=2
+print(x)
+
+# conmparison greater then equal
+
+x=5
+x = x>=6
+print(x)
+
+# comparison not equal to 
+x=5
+x =x != 6
+print(x)
+
+#logicl operator and 
+
+x=3
+x= x<5 and x<2
+print(x)
+
+#logical or operator
+
+x=6
+x= x<10 or x<5
+print(x)
+
+
+# logical not operator
+
+x=10
+x= (not(x>12 and x>8))
+print(x) 
