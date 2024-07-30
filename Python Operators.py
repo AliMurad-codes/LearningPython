2774f7a258865dc76cb782b4361c76f96c9a3de6
AliMurad-codes <ranaali0601@gmail.com>
2024-07-30 17:32:38 +0500

    Changes

--- Python Operators.py
+++ Python Operators.py
@@ -3,78 +3,75 @@ print("hello, python!")
 
 # Addition Operator
 
-print("2+2 =", (2+2))
+print("2+2 =", (2 + 2))
 
 #subtraction operator
-print("3-2=", (3-2))
+print("3-2=", (3 - 2))
 
 # multiplication
-print("3*3=",(3*3))
+print("3*3=", (3 * 3))
 
 # division 
-print("4/2=",(4/2))
+print("4/2=", (4 / 2))
 
 #modulus
-print("9/2=",(9%2))
+print("9/2=", (9 % 2))
 
 #increment
-x=1
-x+=1
+x = 1
+x += 1
 print(x)
 
 #decrement 
-x=4
-x-=1
+x = 4
+x -= 1
 print(x)
 
 #decrement  by divide
-x=8
-x/=4
+x = 8
+x /= 4
 print(x)
 
 #decrement by multiply
-x=3
-x*=3
+x = 3
+x *= 3
 print(x)
 
 # camparison(==)
-x=2
-print(x,x==2)
+x = 2
+print(x, x == 2)
 
 # comparison less then equal 
 
-x=3
-x = x<=2
+x = 3
+x = x <= 2
 print(x)
 
 # conmparison greater then equal
 
-x=5
-x = x>=6
+x = 5
+x = x >= 6
 print(x)
 
 # comparison not equal to 
-x=5
-x =x != 6
+x = 5
+x = x != 6
 print(x)
 
-#logicl operator and 
+# logicl operator and
 
-x=3
-x= x<5 and x<2
+x = 3
+x = x < 5 and x < 2
 print(x)
 
-#logical or operator
+# logical or operator
 
-x=6
-x= x<10 or x<5
+x = 6
+x = x < 10 or x < 5
 print(x)
-
 
 # logical not operator
 
-x=10
-x= (not(x>12 and x>8))
-print(x) 
-
-#Test Comment
+x = 10
+x = (not (x > 12 and x > 8))
+print(x)
